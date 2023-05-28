27cce496991192fc70f1834a105ab5a2d8f734c9
SM Hussain <hussain_mazher@yahoo.com>
2023-05-29 03:02:07 +0530

    08 practice (#4)
    * 07-my practice
    
    * 08-My practice

--- 08-applying-material-design-to-compose/projects/starter/app/src/main/java/com/yourcompany/android/jetnotes/viewmodel/MainViewModel.kt
+++ 08-applying-material-design-to-compose/projects/starter/app/src/main/java/com/yourcompany/android/jetnotes/viewmodel/MainViewModel.kt
@@ -38,6 +38,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
 import com.yourcompany.android.jetnotes.data.repository.Repository
+import com.yourcompany.android.jetnotes.domain.model.ColorModel
 import com.yourcompany.android.jetnotes.domain.model.NoteModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -62,12 +63,39 @@ class MainViewModel(private val repository: Repository) : ViewModel() {
   private var _selectedNotes = MutableStateFlow<List<NoteModel>>(listOf())
   val selectedNotes: LiveData<List<NoteModel>> = _selectedNotes.asLiveData()
 
+  private var _noteEntry = MutableStateFlow<NoteModel>(NoteModel())
+  val noteEntry: LiveData<NoteModel> = _noteEntry.asLiveData()
+
+  val colors: LiveData<List<ColorModel>> by lazy {
+    repository.getAllColors().asLiveData()
+  }
+
+  fun onNoteEntryChange(note: NoteModel) {
+    _noteEntry.value = note
+  }
+
+  fun saveNote(note: NoteModel) {
+    viewModelScope.launch(Dispatchers.Default) {
+      repository.insertNote(note)
+
+      withContext(Dispatchers.Main) {
+        _noteEntry.value = NoteModel()
+      }
+    }
+  }
+
+  fun moveNoteToTrash(note: NoteModel) {
+    viewModelScope.launch(Dispatchers.Default) {
+      repository.moveNoteToTrash(note.id)
+    }
+  }
+
   fun onCreateNewNoteClick() {
-    // TODO - Open SaveNoteScreen
+    _noteEntry.value = NoteModel()
   }
 
   fun onNoteClick(note: NoteModel) {
-    // TODO - Open SaveNoteScreen in Edit mode
+    _noteEntry.value = note
   }
 
   fun onNoteCheckedChange(note: NoteModel) {
